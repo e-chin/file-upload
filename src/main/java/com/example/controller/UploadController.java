eeb95f9dfc0227b283764ff45d6fe8ed7d4cce8c
Eric Chin <e_chin@gcitmail.com>
2018-04-05 20:04:22 -0400

    Refactored unused code

--- src/main/java/com/example/controller/UploadController.java
+++ src/main/java/com/example/controller/UploadController.java
@@ -8,12 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartException;
 
 import com.example.dao.FileDAO;
 import com.example.entity.tempFile;
@@ -48,11 +46,6 @@ public class UploadController {
 			return "Upload failed. File name or file path not provided.";
 		}
 		
-	}
-	
-	@ExceptionHandler(MultipartException.class)
-	public void handleMultipartException(MultipartException e) {
-		logger.info("Upload failed");
 	}
 
 }
